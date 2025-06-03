4adbd534e72167305c61b362e3634a15cf18af43
Rafi-Dev402 <rafi.ahmmed920@gmail.com>
2025-06-04 00:26:29 +0600

    Add a post api

---
+++ index.js
@@ -0,0 +1,62 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 7000;
+
+// Middlewear
+app.use(cors());
+app.use(express.json());
+
+// mongoDB
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@cluster0.wsg3r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+
+    const queryCollection = client.db("QueryNestCollection").collection("queries");
+
+    // Add Query
+    app.post('/add-query', async (req, res) => {
+      const query = req.body
+
+      const result = await queryCollection.insertOne(query)
+      res.send(result)
+    })
+
+    // await client.connect();
+    // await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+  res.send("This server is for QueryNest");
+});
+
+app.listen(port, () => {
+  console.log(`The Port is now running in ${port}`);
+});
+
+
+// queryNest246810KTd
+// Query-Nest
